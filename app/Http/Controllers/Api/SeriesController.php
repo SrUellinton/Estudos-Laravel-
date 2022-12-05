d25bed6e6a74a22ed8e9597fd0b4ce0e208a27f9
SrUellinton <uellinton.19@gmail.com>
2022-12-04 22:28:06 -0300

    ajustes

---
+++ app/Http/Controllers/Api/SeriesController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\SeriesFormRequest;
+use App\Models\Series;
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller
+{
+    public function index()
+    {
+        return Series::all();
+    }
+
+    public function store(SeriesFormRequest $request)
+    {
+        dd($request->all());
+    }
+}
